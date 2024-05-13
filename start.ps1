bafcaa697d18c7aa710018b5aecc3a7344624623
Jakub Jezek <jakubjezek001@gmail.com>
2024-05-13 16:27:12 +0200

    Add start script with functions for installing Poetry and setting environment variables.
    - Added a start script to handle different runtime targets like installation and setting environment variables.
    - Included functions for installing Poetry, changing directories, restoring directories, and handling main logic based on input arguments.

---
+++ start.ps1
@@ -0,0 +1,122 @@
+# Receive first positional argument
+$FunctionName=$ARGS[0]
+$arguments=@()
+if ($ARGS.Length -gt 1) {
+    $arguments = $ARGS[1..($ARGS.Length - 1)]
+}
+
+$poetry_verbosity="-vv"
+
+$current_dir = Get-Location
+$repo_root_rel = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$repo_root = (Get-Item $repo_root_rel).FullName
+
+
+function Default-Func {
+    Write-Host ""
+    Write-Host "LabLib library"
+    Write-Host ""
+    Write-Host "Usage: ./start.ps1 [target]"
+    Write-Host ""
+    Write-Host "Runtime targets:"
+    Write-Host "  install                       Install Poetry and update venv by lock file."
+    Write-Host "  set-env                       Set all env vars in .env file."
+    Write-Host ""
+}
+
+function Exit-WithCode($exitcode) {
+   # Only exit this host process if it's a child of another PowerShell parent process...
+   $parentPID = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId=$PID" | Select-Object -Property ParentProcessId).ParentProcessId
+   $parentProcName = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId=$parentPID" | Select-Object -Property Name).Name
+   if ('powershell.exe' -eq $parentProcName) { $host.SetShouldExit($exitcode) }
+   Restore-Cwd
+   exit $exitcode
+}
+
+function Install-Poetry() {
+    Write-Host ">>> Installing Poetry ... "
+    $python = "python"
+    if (Get-Command "pyenv" -ErrorAction SilentlyContinue) {
+        if (-not (Test-Path -PathType Leaf -Path "$($repo_root)\.python-version")) {
+            $result = & pyenv global
+            if ($result -eq "no global version configured") {
+                Write-Host "!!! ", "Using pyenv but having no local or global version of Python set."
+                Exit-WithCode 1
+            }
+        }
+        $python = & pyenv which python
+    }
+
+    $env:POETRY_HOME="$repo_root\.poetry"
+    $env:POETRY_VERSION="1.3.2"
+    (Invoke-WebRequest -Uri https://install.python-poetry.org/ -UseBasicParsing).Content | & $($python) -
+}
+
+function Change-Cwd() {
+    Set-Location -Path $repo_root
+}
+
+function Restore-Cwd() {
+    Set-Location -Path $current_dir
+}
+
+function install {
+    # install dependencies for tool
+     if (-not (Test-Path -PathType Container -Path "$($env:POETRY_HOME)\bin")) {
+        Install-Poetry
+    }
+
+    Change-Cwd
+
+    Write-Host ">>> ", "Poetry config ... "
+    & "$env:POETRY_HOME\bin\poetry" install --no-interaction --no-root --ansi  $poetry_verbosity
+}
+
+function set_env {
+    # set all env vars in .env file
+    if (-not (Test-Path "$($repo_root)\.env")) {
+        Write-Host "!!! ", ".env file must be prepared!" -ForegroundColor red
+        Exit-WithCode 1
+    } else {
+        $content = Get-Content -Path "$($repo_root)\.env" -Encoding UTF8 -Raw
+        foreach($line in $content.split()) {
+            if ($line){
+                $parts = $line.split("=")
+                $varName = $parts[0]
+                $value = $parts[1]
+                Write-Host "Setting $varName with $value"
+                Set-Item "env:$varName" $value
+            }
+        }
+        $env:PYTHONPATH=$repo_root
+    }
+}
+
+function main {
+    if ($FunctionName -eq $null)
+    {
+        Default-Func
+        return
+    }
+    $FunctionName = $FunctionName.ToLower() -replace "\W"
+    if ($FunctionName -eq "install") {
+        Change-Cwd
+        install
+    } elseif ($FunctionName -eq "test") {
+        Change-Cwd
+        set_env
+        & "$env:POETRY_HOME\bin\poetry" run pytest -x --capture=sys -W ignore::DeprecationWarning @arguments
+    } elseif ($FunctionName -eq "setenv") {
+        Change-Cwd
+        set_env
+    } else {
+        Write-Host "Unknown function \"$FunctionName\""
+        Default-Func
+    }
+    Restore-Cwd
+}
+
+# Force POETRY_HOME to this directory
+$env:POETRY_HOME = "$repo_root\.poetry"
+
+main
