1b06a14912b462fc51dc995a6be5476a68462f5b
doerp <me@doerp.cool>
2024-05-15 20:23:35 +0200

    removes refactored code

--- lablib/lib/imageio.py
+++ lablib/lib/imageio.py
@@ -236,51 +236,6 @@ class SequenceInfo(ImageIOBase):
     def __init__(self, path: Path, imageinfos: List[ImageInfo]):
         super().__init__(path=path, imageinfos=imageinfos)
 
-    def _get_file_splits(self, file_name: str) -> None:
-        head, ext = os.path.splitext(file_name)
-        frame = int(re.findall(r"\d+$", head)[0])
-        return head.replace(str(frame), ""), frame, ext
-
-    def _get_length(self) -> int:
-        return int(self.frame_end) - int(self.frame_start) + 1
-
-    def compute_all(self, scan_dir: str) -> List:
-        files = os.listdir(scan_dir)
-        sequenced_files = []
-        matched_files = []
-        for f in files:
-            head, tail = os.path.splitext(f)
-            matches = re.findall(r"\d+$", head)
-            if matches:
-                sequenced_files.append(f)
-                matched_files.append(head.replace(matches[0], ""))
-        matched_files = list(set(matched_files))
-
-        results = []
-        for m in matched_files:
-            seq = SequenceInfo()
-            for sf in sequenced_files:
-                if m in sf:
-                    seq.frames.append(os.path.join(scan_dir, sf).replace("\\", "/"))
-
-            head, frame, ext = self._get_file_splits(seq.frames[0])
-            seq.path = os.path.abspath(scan_dir).replace("\\", "/")
-            seq.frame_start = frame
-            seq.frame_end = self._get_file_splits(seq.frames[-1])[1]
-            seq.head = os.path.basename(head)
-            seq.tail = ext
-            seq.padding = len(str(frame))
-            seq.hash_string = "{}#{}".format(os.path.basename(head), ext)
-            seq.format_string = "{}%0{}d{}".format(
-                os.path.basename(head), len(str(frame)), ext
-            )
-            results.append(seq)
-
-        return results
-
-    def compute_longest(self, scan_dir: str) -> SequenceInfo:
-        return self.compute_all(scan_dir=scan_dir)[0]
-
     @classmethod
     def scan(cls, directory: str | Path) -> List[SequenceInfo]:
         cls.log.info(f"Scanning {directory}")
