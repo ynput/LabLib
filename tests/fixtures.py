5ebd6e0a7ee390aaf7121b0147c9d23cd0523e67
doerp <me@doerp.cool>
2024-05-16 22:01:20 +0200

    inits crop, adds crop_op_data fixture

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 import pytest
 
 from pathlib import Path
@@ -8,20 +9,47 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
 
+def _get_reposition_operators(file: Union[Path, str]) -> List[dict]:
+    if isinstance(file, str):
+        file = Path(file)
+
+    ops = []
+    with file.open("r") as f:
+        data = json.load(f)
+        for k, v in data.items():
+            if isinstance(v, dict):
+                if "OCIO" not in k:
+                    ops.append(v)
+    return ops
+
+
 @pytest.fixture
 def transform_op_data():
     file = Path(
         "resources/public/effectPlateMain/v000/BLD_010_0010_effectPlateMain_v000.json"
     )
 
-    with file.open("r") as f:
-        data = json.load(f)
-
     transforms = []
-    for key in data.keys():
-        if "Transform" in key:
-            if "OCIO" not in key:
-                transforms.append(data[key])
+    ops = _get_reposition_operators(file)
+    for op in ops:
+        if op.get("class") == "Transform":
+            transforms.append(op)
 
     for transform in transforms:
         yield transform
+
+
+@pytest.fixture
+def crop_op_data():
+    file = Path(
+        "resources/public/effectPlateMain/v001/a01vfxd_sh020_effectPlateP01_v002.json"
+    )
+
+    crops = []
+    ops = _get_reposition_operators(file)
+    for op in ops:
+        if op.get("class") == "Crop":
+            crops.append(op)
+
+    for crop in crops:
+        yield crop
--- tests/test_reposition.py
+++ tests/test_reposition.py
@@ -1,5 +1,5 @@
-from lablib.operators import Transform
-from fixtures import transform_op_data
+from lablib.operators import Transform, Crop
+from fixtures import transform_op_data, crop_op_data
 
 
 def test_Transform(transform_op_data):
@@ -21,4 +21,17 @@ def test_Transform(transform_op_data):
         "--rotate 0.0",
         "--scale 0.0 0.0",
         "--center 0.0 0.0",
+    ]
+
+
+def test_Crop(crop_op_data):
+    crop = Crop.from_node_data(crop_op_data)
+    oiio_args = crop.to_oiio_args()
+
+    # assert fields
+    assert crop.box == [0, 0, 1920, 1080]
+
+    # assert argument output
+    assert oiio_args == [
+        "--crop 0,0,1920,1080",
     ]
