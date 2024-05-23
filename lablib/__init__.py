c3839d44068c85d6236feb2c893faaa851f8dd86
Jakub Jezek <jakubjezek001@gmail.com>
2024-05-23 07:44:33 +0200

    Update package metadata
    Update package metadata with version, author, title, homepage,
    description, license, and keywords.
    
    Added package metadata for LabLib including version, author,
    title, homepage, description, license, and keywords.

--- lablib/__init__.py
+++ lablib/__init__.py
@@ -20,3 +20,12 @@ __all__ = [
     "SlateRenderer",
     "BasicRenderer",
 ]
+
+
+__version__ = "0.1.0-dev.1"
+__author__ = "YNPUT, s.r.o. <team@ynput.io>"
+__title__ = "LabLib"
+__homepage__ = "https://github.com/ynput/lablib"
+__description__ = "LabLib is a collection of tools and utilities for VFX and Animation production."
+__license__ = "MIT"
+__keywords__ = "vfx animation production tools utilities"
