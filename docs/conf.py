5c0532908d46578ab4823b1283368a7026237c26
doerp <me@doerp.cool>
2024-06-27 23:09:08 +0200

    replaces hardcoded references

--- docs/conf.py
+++ docs/conf.py
@@ -2,8 +2,8 @@ import lablib
 
 project = "LabLib"
 copyright = "2024, maxpareschi, jakubjezek001, doerp"
-author = "maxpareschi, jakubjezek001, doerp"
-release = "0.1.0-dev.1"
+author = lablib.__author__
+release = lablib.__version__
 
 extensions = ["sphinx.ext.autodoc"]
 templates_path = ["_templates"]
