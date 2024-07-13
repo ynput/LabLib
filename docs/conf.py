d4bc56790db1697e0d5d8c8689746f21db98fa4d
doerp <me@doerp.cool>
2024-07-13 23:52:43 +0200

    adds viewcode sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,12 @@ copyright = "2024, maxpareschi, jakubjezek001, doerp"
 author = lablib.__author__
 release = lablib.__version__
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
