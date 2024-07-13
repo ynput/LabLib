60786fce034a4a4679cbc6929bf099c6b9d38917
doerp <me@doerp.cool>
2024-07-13 22:13:42 +0200

    adds sphinx todo extension, removes revitron theme

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,10 @@ copyright = "2024, maxpareschi, jakubjezek001, doerp"
 author = lablib.__author__
 release = lablib.__version__
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "revitron_sphinx_theme"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.todo", "sphinx.ext.napoleon"]
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 html_theme = "furo"
+
+todo_include_todos = True
