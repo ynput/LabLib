5229c08fe51f9c474bb010acc82675da8fac1876
doerp <me@doerp.cool>
2024-07-13 22:14:16 +0200

    include Burnin and Codec for autodocs

--- lablib/renderers/__init__.py
+++ lablib/renderers/__init__.py
@@ -1,9 +1,11 @@
 """Renderer module combining Operators, Generators and Processors for final image or video file."""
 
-from .basic import BasicRenderer
+from .basic import BasicRenderer, Burnin, Codec
 from .slate_render import SlateRenderer
 
 __all__ = [
     "BasicRenderer",
+    "Burnin",
+    "Codec",
     "SlateRenderer",
 ]
