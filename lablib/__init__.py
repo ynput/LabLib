5a41ed0efa7a82e1c15151fd9248ceb2845eb5a9
doerp <me@doerp.cool>
2024-07-19 21:59:31 +0200

    bump version to 0.1.0

--- lablib/__init__.py
+++ lablib/__init__.py
@@ -25,10 +25,12 @@ __all__ = [
 ]
 
 
-__version__ = "0.1.0-dev.2"
+__version__ = "0.1.0"
 __author__ = "YNPUT, s.r.o. <team@ynput.io>"
 __title__ = "LabLib"
 __homepage__ = "https://github.com/ynput/lablib"
-__description__ = "LabLib is a collection of tools and utilities for VFX and Animation production."
+__description__ = (
+    "LabLib is a collection of tools and utilities for VFX and Animation production."
+)
 __license__ = "MIT"
 __keywords__ = "vfx animation production tools utilities"
