3033d50983655c3c64e542cb70a2826403261bf1
Jakub Jezek <jakubjezek001@gmail.com>
2024-07-30 17:28:00 +0200

    WIP testing for AYONHieroEffectsFileProcessor.
    - Added new test parameter structure with file paths and expected results.
    - Adjusted test method to accept kwargs and results instead of just a path.
    - Updated processor initialization and debug log messages.
    - Modified assertions to compare processor attributes with expected results.

--- tests/test_processors_ayon_hiero_effect_file.py
+++ tests/test_processors_ayon_hiero_effect_file.py
@@ -6,26 +6,91 @@ from lablib.processors import AYONHieroEffectsFileProcessor
 
 log = logging.getLogger(__name__)
 
+ABS_MODULE_PATH = Path(__file__).resolve().parent.parent.as_posix()
+
 
 class TestAYONHieroEffectFileProcessor:
 
     @pytest.mark.parametrize(
-        "path",
+        "kwargs,results",
         [
-            "resources/public/effectPlateMain/v000/"
-            "BLD_010_0010_effectPlateMain_v000.json"
-        ]
+            (
+                {
+                    "filepath": Path(
+                        "resources/public/effectPlateMain/v000/"
+                        "BLD_010_0010_effectPlateMain_v000.json"
+                    ),
+                    "target_path": Path(
+                        "published/ociolookMain/v006/"
+                        "john_ociolookMain_v006.json"
+                    ),
+                    "logger": log,
+                },
+                {
+                    "expected_path": Path(
+                        "resources/public/ociolookMain/v005/"
+                        "jp03_john_ociolookMain_v005.json"
+                    ),
+                    "expected_length": 5,
+                    "expected_target_file": "john_ociolookMain_v006.cube",
+                    "expected_cmd": [
+                        "--colorconvert",
+                        "ACES - ACEScg",
+                        "Output - sRGB",
+                        "--ociofiletransform",
+                        "john_ociolookMain_v006.cube",
+                        "--colorconvert",
+                        "ACES - ACEScc",
+                        "ACES - ACEScg",
+                    ],
+                },
+            ),
+            (
+                {
+                    "filepath": Path(
+                        "resources/public/effectPlateMain/v000/"
+                        "BLD_010_0010_effectPlateMain_v000.json"
+                    )
+                },
+                {
+                    "expected_path": Path(
+                        "resources/public/ociolookMain/v005/"
+                        "jp03_john_ociolookMain_v005.json"
+                    ),
+                    "expected_length": 5,
+                    "expected_target_file": (
+                        f"{ABS_MODULE_PATH}/resources/public/ociolookMain/"
+                        "v005/jp03_john_ociolookMain_v005.cube"
+                    ),
+                    "expected_cmd": [
+                        "--colorconvert",
+                        "ACES - ACEScg",
+                        "Output - sRGB",
+                        "--ociofiletransform",
+                        (
+                            f"{ABS_MODULE_PATH}/resources/public/ociolookMain/"
+                            "v005/jp03_john_ociolookMain_v005.cube"
+                        ),
+                        "--colorconvert",
+                        "ACES - ACEScc",
+                        "ACES - ACEScg",
+                    ],
+                },
+            ),
+        ],
     )
-    def test_EffectsFileProcessor(self, path):  # noqa: F811
+    def test_EffectsFileProcessor(self, kwargs, results):
 
-        effect_processor = AYONHieroEffectsFileProcessor(
-            Path(path)
-        )
-        log.debug(f"{effect_processor = }")
-        effect_processor.load()
+        processor = AYONHieroEffectsFileProcessor(**kwargs)
+        log.debug(f"Processor: {processor = }")
 
-        log.debug(effect_processor.color_operators)
-        assert effect_processor.filepath == Path(path)
-        # 5 because CDLTransform is converted into CDLTransform and
+        log.debug(processor.operator)
+        assert processor.filepath == results["expected_path"]
+        assert processor.get_oiiotool_cmd() == results["expected_cmd"]
         # FileTransform
-        assert len(effect_processor.color_operators) == 5
+        assert len(processor.operator.to_ocio_obj()) == results[
+            "expected_length"]
+        assert (
+            processor.operator.ocioLookItems[0]["file"]
+            == results["expected_target_file"]
+        )
