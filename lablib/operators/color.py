44ad984ecee967c0507361fc4aa27df7d720644c
doerp <me@doerp.cool>
2024-10-23 19:15:27 +0200

    refactors classes place in code indicating inheritance better

--- lablib/operators/color.py
+++ lablib/operators/color.py
@@ -45,75 +45,6 @@ class ColorOperator(BaseOperator):
 
 
 @dataclass
-class OCIOFileTransform(ColorOperator):
-    """Class for handling OCIO FileTransform effects.
-
-    Note:
-        Reads Foundry Hiero Timeline soft effect node class.
-
-    Attributes:
-        file (str): Path to the LUT file.
-        cccid (str): Path to the cccid file.
-        direction (int): The direction. Defaults to 0.
-        interpolation (str): The interpolation. Defaults to "linear".
-    """
-
-    file: str = ""
-    cccid: str = ""
-    direction: int = 0
-    interpolation: str = "linear"
-
-    def to_ocio_obj(self) -> List[OCIO.FileTransform]:
-        """Converts the object to native OCIO object.
-
-        Returns:
-            List[OCIO.FileTransform]: The OCIO FileTransform object in a list.
-        """
-        # define direction
-        direction = get_direction(self.direction)
-
-        # define interpolation
-        interpolation = get_interpolation(self.interpolation)
-
-        return [
-            OCIO.FileTransform(
-                src=Path(self.file).as_posix(),
-                cccId=self.cccid,
-                direction=direction,
-                interpolation=interpolation,
-            )
-        ]
-
-    @classmethod
-    def from_node_data(cls, data) -> "OCIOFileTransform":
-        """Create :obj:`OCIOFileTransform` from node data.
-
-        Note:
-            Reads Foundry Hiero Timeline soft effect node data.
-
-            Would it be cool if we'd had a way to interface other DCC node data?
-            Would they even be so much different?
-
-        Args:
-            data (dict): The node data. List of expected but not required keys:
-                - file (str): Path to the LUT file.
-                - cccid (str): Path to the cccid file.
-                - direction (int): The direction. Defaults to 0.
-                - interpolation (str): The interpolation. Defaults to "linear".
-
-
-        Returns:
-            OCIOFileTransform: The OCIOFileTransform object.
-        """
-        return cls(
-            file=data.get("file", ""),
-            cccid=data.get("cccid", ""),
-            direction=data.get("direction", 0),
-            interpolation=data.get("interpolation", "linear"),
-        )
-
-
-@dataclass
 class OCIOColorSpace(ColorOperator):
     """Foundry Hiero Timeline soft effect node class.
 
@@ -155,120 +86,6 @@ class OCIOColorSpace(ColorOperator):
             in_colorspace=data.get("in_colorspace", ""),
             out_colorspace=data.get("out_colorspace", ""),
         )
-
-
-@dataclass
-class OCIOCDLTransform(ColorOperator):
-    """Foundry Hiero Timeline soft effect node class.
-
-    Note:
-        Since this node class combines two of OCIO classes (FileTransform and
-        CDLTransform), we will separate them here within
-        :obj:`OCIOCDLTransform.to_ocio_obj()`.
-
-    Attributes:
-        file (Optional[str]): Path to the LUT file.
-        direction (int): The direction. Defaults to 0.
-        cccid (str): The cccid. Defaults to "".
-        offset (List[float]): The offset. Defaults to [0.0, 0.0, 0.0].
-        power (List[float]): The power. Defaults to [1.0, 1.0, 1.0].
-        slope (List[float]): The slope. Defaults to [0.0, 0.0, 0.0].
-        saturation (float): The saturation. Defaults to 1.0.
-        interpolation (str): The interpolation. Defaults to "linear".
-    """
-
-    file: Optional[str] = None
-    direction: int = 0
-    cccid: str = ""
-    offset: List[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
-    power: List[float] = field(default_factory=lambda: [1.0, 1.0, 1.0])
-    slope: List[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
-    saturation: float = 1.0
-    interpolation: str = "linear"
-
-    def to_ocio_obj(self) -> List[Union[OCIO.FileTransform, OCIO.CDLTransform]]:  # noqa: E501
-        """Returns native OCIO FileTransform and CDLTransform object.
-
-        Returns:
-            List[Union[OCIO.FileTransform, OCIO.CDLTransform]]: The OCIO
-                CDLTransform and FileTransform object in a list.
-                If file is not provided, only CDLTransform will be returned.
-        """
-        effects = []
-
-        # define direction
-        direction = get_direction(self.direction)
-
-        if self.file:
-            # define interpolation
-            interpolation = get_interpolation(self.interpolation)
-            lut_file = Path(self.file)
-
-            effects.append(
-                OCIO.FileTransform(
-                    src=lut_file.as_posix(),
-                    cccId=(self.cccid or "0"),
-                    interpolation=interpolation,
-                    direction=direction,
-                )
-            )
-
-        effects.append(
-            OCIO.CDLTransform(
-                slope=self.slope,
-                offset=self.offset,
-                power=self.power,
-                sat=self.saturation,
-                direction=direction,
-            )
-        )
-
-        return effects
-
-    @classmethod
-    def from_node_data(cls, data) -> "OCIOCDLTransform":
-        """Create :obj:`OCIOCDLTransform` from node data.
-
-        Args:
-            data (dict): The node data. List of expected but not required keys:
-                - file (str): Path to the LUT file.
-                - direction (int): The direction.
-                    Defaults to 0.
-                - cccid (str): The cccid.
-                    Defaults to "".
-                - offset (List[float]): The offset.
-                    Defaults to [0.0, 0.0, 0.0].
-                - power (List[float]): The power.
-                    Defaults to [1.0, 1.0, 1.0].
-                - slope (List[float]): The slope.
-                    Defaults to [0.0, 0.0, 0.0].
-                - saturation (float): The saturation.
-                    Defaults to 1.0.
-                - interpolation (str): The interpolation.
-                    Defaults to "linear".
-
-        Returns:
-            OCIOCDLTransform: The OCIOCDLTransform object.
-        """
-        if data.get("file"):
-            return cls(
-                file=data.get("file", ""),
-                interpolation=data.get("interpolation", "linear"),
-                direction=data.get("direction", 0),
-                offset=data.get("offset", [0.0, 0.0, 0.0]),
-                power=data.get("power", [1.0, 1.0, 1.0]),
-                slope=data.get("slope", [0.0, 0.0, 0.0]),
-                saturation=data.get("saturation", 1.0),
-                cccid=data.get("cccid", ""),
-            )
-        else:
-            return cls(
-                direction=data.get("direction", 0),
-                offset=data.get("offset", [0.0, 0.0, 0.0]),
-                power=data.get("power", [1.0, 1.0, 1.0]),
-                slope=data.get("slope", [0.0, 0.0, 0.0]),
-                saturation=data.get("saturation", 1.0),
-            )
 
 
 @dataclass
@@ -388,3 +205,186 @@ class AYONOCIOLookProduct(ColorOperator):
             ocioLookItems=data.get("ocioLookItems", []),
             ocioLookWorkingSpace=data.get("ocioLookWorkingSpace", {}),
         )
+
+
+@dataclass
+class OCIOFileTransform(ColorOperator):
+    """Class for handling OCIO FileTransform effects.
+
+    Note:
+        Reads Foundry Hiero Timeline soft effect node class.
+
+    Attributes:
+        file (str): Path to the LUT file.
+        cccid (str): Path to the cccid file.
+        direction (int): The direction. Defaults to 0.
+        interpolation (str): The interpolation. Defaults to "linear".
+    """
+
+    file: str = ""
+    cccid: str = ""
+    direction: int = 0
+    interpolation: str = "linear"
+
+    def to_ocio_obj(self) -> List[OCIO.FileTransform]:
+        """Converts the object to native OCIO object.
+
+        Returns:
+            List[OCIO.FileTransform]: The OCIO FileTransform object in a list.
+        """
+        # define direction
+        direction = get_direction(self.direction)
+
+        # define interpolation
+        interpolation = get_interpolation(self.interpolation)
+
+        return [
+            OCIO.FileTransform(
+                src=Path(self.file).as_posix(),
+                cccId=self.cccid,
+                direction=direction,
+                interpolation=interpolation,
+            )
+        ]
+
+    @classmethod
+    def from_node_data(cls, data) -> "OCIOFileTransform":
+        """Create :obj:`OCIOFileTransform` from node data.
+
+        Note:
+            Reads Foundry Hiero Timeline soft effect node data.
+
+            Would it be cool if we'd had a way to interface other DCC node data?
+            Would they even be so much different?
+
+        Args:
+            data (dict): The node data. List of expected but not required keys:
+                - file (str): Path to the LUT file.
+                - cccid (str): Path to the cccid file.
+                - direction (int): The direction. Defaults to 0.
+                - interpolation (str): The interpolation. Defaults to "linear".
+
+
+        Returns:
+            OCIOFileTransform: The OCIOFileTransform object.
+        """
+        return cls(
+            file=data.get("file", ""),
+            cccid=data.get("cccid", ""),
+            direction=data.get("direction", 0),
+            interpolation=data.get("interpolation", "linear"),
+        )
+
+
+@dataclass
+class OCIOCDLTransform(ColorOperator):
+    """Foundry Hiero Timeline soft effect node class.
+
+    Note:
+        Since this node class combines two of OCIO classes (FileTransform and
+        CDLTransform), we will separate them here within
+        :obj:`OCIOCDLTransform.to_ocio_obj()`.
+
+    Attributes:
+        file (Optional[str]): Path to the LUT file.
+        direction (int): The direction. Defaults to 0.
+        cccid (str): The cccid. Defaults to "".
+        offset (List[float]): The offset. Defaults to [0.0, 0.0, 0.0].
+        power (List[float]): The power. Defaults to [1.0, 1.0, 1.0].
+        slope (List[float]): The slope. Defaults to [0.0, 0.0, 0.0].
+        saturation (float): The saturation. Defaults to 1.0.
+        interpolation (str): The interpolation. Defaults to "linear".
+    """
+
+    file: Optional[str] = None
+    direction: int = 0
+    cccid: str = ""
+    offset: List[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
+    power: List[float] = field(default_factory=lambda: [1.0, 1.0, 1.0])
+    slope: List[float] = field(default_factory=lambda: [0.0, 0.0, 0.0])
+    saturation: float = 1.0
+    interpolation: str = "linear"
+
+    def to_ocio_obj(self) -> List[Union[OCIO.FileTransform, OCIO.CDLTransform]]:  # noqa: E501
+        """Returns native OCIO FileTransform and CDLTransform object.
+
+        Returns:
+            List[Union[OCIO.FileTransform, OCIO.CDLTransform]]: The OCIO
+                CDLTransform and FileTransform object in a list.
+                If file is not provided, only CDLTransform will be returned.
+        """
+        effects = []
+
+        # define direction
+        direction = get_direction(self.direction)
+
+        if self.file:
+            # define interpolation
+            interpolation = get_interpolation(self.interpolation)
+            lut_file = Path(self.file)
+
+            effects.append(
+                OCIO.FileTransform(
+                    src=lut_file.as_posix(),
+                    cccId=(self.cccid or "0"),
+                    interpolation=interpolation,
+                    direction=direction,
+                )
+            )
+
+        effects.append(
+            OCIO.CDLTransform(
+                slope=self.slope,
+                offset=self.offset,
+                power=self.power,
+                sat=self.saturation,
+                direction=direction,
+            )
+        )
+
+        return effects
+
+    @classmethod
+    def from_node_data(cls, data) -> "OCIOCDLTransform":
+        """Create :obj:`OCIOCDLTransform` from node data.
+
+        Args:
+            data (dict): The node data. List of expected but not required keys:
+                - file (str): Path to the LUT file.
+                - direction (int): The direction.
+                    Defaults to 0.
+                - cccid (str): The cccid.
+                    Defaults to "".
+                - offset (List[float]): The offset.
+                    Defaults to [0.0, 0.0, 0.0].
+                - power (List[float]): The power.
+                    Defaults to [1.0, 1.0, 1.0].
+                - slope (List[float]): The slope.
+                    Defaults to [0.0, 0.0, 0.0].
+                - saturation (float): The saturation.
+                    Defaults to 1.0.
+                - interpolation (str): The interpolation.
+                    Defaults to "linear".
+
+        Returns:
+            OCIOCDLTransform: The OCIOCDLTransform object.
+        """
+        if data.get("file"):
+            return cls(
+                file=data.get("file", ""),
+                interpolation=data.get("interpolation", "linear"),
+                direction=data.get("direction", 0),
+                offset=data.get("offset", [0.0, 0.0, 0.0]),
+                power=data.get("power", [1.0, 1.0, 1.0]),
+                slope=data.get("slope", [0.0, 0.0, 0.0]),
+                saturation=data.get("saturation", 1.0),
+                cccid=data.get("cccid", ""),
+            )
+        else:
+            return cls(
+                direction=data.get("direction", 0),
+                offset=data.get("offset", [0.0, 0.0, 0.0]),
+                power=data.get("power", [1.0, 1.0, 1.0]),
+                slope=data.get("slope", [0.0, 0.0, 0.0]),
+                saturation=data.get("saturation", 1.0),
+            )
