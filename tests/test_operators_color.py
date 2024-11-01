88b7447fbb5d47debaa70c019f503ff917b20c2f
Jakub Jezek <jakubjezek001@gmail.com>
2024-11-01 14:35:08 +0100

    Refactor color operator testing

--- tests/test_operators_color.py
+++ tests/test_operators_color.py
@@ -30,21 +30,17 @@ class TestColorOperators:
     )
     def test_OCIOFileTransform(self, data: dict):
         lut_file = Path(data["file"]).as_posix()
-        lut = OCIOFileTransform.from_node_data(data)
-        lut_obj = lut.to_ocio_obj()
+        op_obj = OCIOFileTransform.from_node_data(data)
+        ocio_obj = op_obj.to_ocio_obj()
         expected_direction = OCIO.TransformDirection.TRANSFORM_DIR_FORWARD
         expected_interpolation = OCIO.Interpolation.INTERP_LINEAR
-        assert len(lut_obj) == 1
-
-        # get the ocio object
-        ocio_obj = lut_obj.pop()
 
         assert ocio_obj.getSrc() == lut_file
         assert ocio_obj.getCCCId() == "TEST_CCCID"
         assert ocio_obj.getDirection() == expected_direction
         assert ocio_obj.getInterpolation() == expected_interpolation
 
-        log.debug(f"{lut = }")
+        log.debug(f"{op_obj = }")
 
     @pytest.mark.parametrize(
         "data",
@@ -56,61 +52,65 @@ class TestColorOperators:
         ],
     )
     def test_OCIOColorSpace(self, data: dict):
-        colorspace = OCIOColorSpace.from_node_data(data)
-        colorspace_obj = colorspace.to_ocio_obj()
-
-        assert len(colorspace_obj) == 1
-        # get the ocio object
-        ocio_obj = colorspace_obj.pop()
+        op_obj = OCIOColorSpace.from_node_data(data)
+        ocio_obj = op_obj.to_ocio_obj()
 
         assert ocio_obj.getSrc() == data["in_colorspace"]
         assert ocio_obj.getDst() == data["out_colorspace"]
 
-        log.debug(f"{colorspace_obj = }")
+        log.debug(f"{ocio_obj = }")
 
     @pytest.mark.parametrize(
-        "data",
+        "data,expected",
         [
-            {
-                "file": (
-                    "resources/public/effectPlateMain/v000/"
-                    "resources/BLD_010_0010.cc"
-                ),
-                "slope": [1.0, 1.0, 1.0],
-                "offset": [0.0, 0.0, 0.0],
-                "power": [1.0, 1.0, 1.0],
-                "saturation": 1.0,
-                "interpolation": "nearest",
-            },
-            {
-                "slope": [1.0, 1.0, 1.0],
-                "offset": [0.0, 0.0, 0.0],
-                "power": [1.0, 1.0, 1.0],
-                "saturation": 1.0,
-            },
+            (
+                {
+                    "file": (
+                        "resources/public/effectPlateMain/v000/"
+                        "resources/BLD_010_0010.cc"
+                    ),
+                    "slope": [1.0, 1.0, 1.0],
+                    "offset": [0.0, 0.0, 0.0],
+                    "power": [1.0, 1.0, 1.0],
+                    "saturation": 1.0,
+                    "interpolation": "nearest",
+                },
+                {
+                    "file": (
+                        "resources/public/effectPlateMain/v000/"
+                        "resources/BLD_010_0010.cc"
+                    ),
+                    "interpolation": OCIO.Interpolation.INTERP_NEAREST,
+                },
+            ),
+            (
+                {
+                    "slope": [1.0, 1.0, 1.0],
+                    "offset": [0.0, 0.0, 0.0],
+                    "power": [1.0, 1.0, 1.0],
+                    "saturation": 1.0,
+                },
+                {
+                    "slope": [1.0, 1.0, 1.0],
+                    "offset": [0.0, 0.0, 0.0],
+                    "power": [1.0, 1.0, 1.0],
+                    "saturation": 1.0,
+                },
+            ),
         ],
     )
-    def test_OCIOCDLTransform(self, data: dict):
-        cdl = OCIOCDLTransform.from_node_data(data)
-        cdl_obj = cdl.to_ocio_obj()
+    def test_OCIOCDLTransform(self, data: dict, expected: dict):
+        op_obj = OCIOCDLTransform.from_node_data(data)
+        ocio_obj = op_obj.to_ocio_obj()
 
-        expected_len = 1
         # sourcery skip: no-conditionals-in-tests
         if data.get("file"):
-            expected_len = 2
-            file_transform = cdl_obj[0]
-            cdl_transform = cdl_obj[1]
-            expected_interpolation = OCIO.Interpolation.INTERP_NEAREST
-            assert file_transform.getSrc() == data["file"]
-            assert file_transform.getInterpolation() == expected_interpolation
+            assert ocio_obj.getSrc() == expected["file"]
+            assert ocio_obj.getInterpolation() == expected["interpolation"]
         else:
-            cdl_transform = cdl_obj[0]
+            assert ocio_obj.getSlope() == expected["slope"]
+            assert ocio_obj.getOffset() == expected["offset"]
+            assert ocio_obj.getPower() == expected["power"]
+            assert ocio_obj.getSat() == expected["saturation"]
 
-        log.debug(f"{cdl_obj = }")
-        assert len(cdl_obj) == expected_len
-        assert cdl_transform.getSlope() == data["slope"]
-        assert cdl_transform.getOffset() == data["offset"]
-        assert cdl_transform.getPower() == data["power"]
-        assert cdl_transform.getSat() == data["saturation"]
-
-        log.debug(f"{cdl_obj = }")
+        log.debug(f"{ocio_obj = }")
